3f9418ece86c698668773d04fba1b465aae5f6e7
codiuscube <cody.a.iddings@gmail.com>
2023-12-22 12:38:20 -0600

    Profile challenge

--- 03-profile-challenge/src/index.js
+++ 03-profile-challenge/src/index.js
@@ -15,24 +15,49 @@ function App() {
 }
 
 function Avatar() {
-  return (
-    <img src="public/wave.jpeg"
-  )
+  return <img className="avatar" src="avatar.jpeg" alt="Cody Iddings" />;
 }
 
 function Intro() {
   return (
-    <p>Profile</p>
-  )
+    <div>
+      <h1>Cody Iddings</h1>
+      <p>
+        I'm a co-founder of Makeshapes. We're on a mission to democratise
+        high-quality team learning. Our unique approach allows organisations to
+        scale learning and culture change in a way not previously possible.
+      </p>
+    </div>
+  );
 }
 
-function Intro() {
+function SkillList() {
   return (
-    <p>Profile</p>
-  )
+    <div className="skill-list">
+      <Skill name="Surfing 🏄" />
+      <Skill name="Dancing 💃" />
+      <Skill name="Cooking 🍳" />
+      <Skill name="Writing 🖋️" />
+      <Skill name="Gaming 🎮" />
+      <Skill name="Daddy 🤪" />
+    </div>
+  );
 }
 
-
+function Skill(props) {
+  console.log(props);
+  const bgColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  const textColor =
+    parseInt(bgColor.replace("#", ""), 16) > 0xffffff / 2 ? "#000" : "#fff";
+  return (
+    <div
+      className="skill"
+      style={{ backgroundColor: bgColor, color: textColor }}
+    >
+      <p>{props.name}</p>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
